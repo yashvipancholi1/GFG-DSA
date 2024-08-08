4a57b06a5d9b5e1de1c75df3b261235c3fe3e515
Yashvi Pancholi <pancholiyashvi@gmail.com>
2024-08-08 11:59:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -13,31 +13,28 @@ class Solution{
         if(n<=1) return 0;
         if(arr[0]==0) return -1;
         
-        int maxR=arr[0];
-        int step = arr[0];
-        int jumps =1;
+        
+        int maxR = arr[0];
+        int jumps = 1;
+        int steps = arr[0];
+        
         
         for(int i=1; i<n; i++){
-            //base condition
-            if( i== n-1){
-                return jumps;
-            }
             
-            maxR = max(maxR, arr[i]+i);
+            if(i==n-1) return jumps;
             
-            step--;
+            maxR = max(maxR, i+arr[i]);
+            steps--;
             
-            if(step==0){
+            if(steps==0){
                 jumps++;
-                
                 if(i>=maxR) return -1;
+                steps = maxR-i;
                 
-                step = maxR-i;
             }
+            
         }
-        
         return -1;
-        
     }
 };
 
